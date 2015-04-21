1ac51215cb9fc5aecee53931aab6f6b7028ebb6a
Camikaz <camille.rouet@insa-lyon.fr>
2015-04-21 21:52:45 +0200

    Creation de classe de tests

--- src/ClasseTestASupprimer.java
+++
@@ -1,9 +0,0 @@
-
-public class ClasseTestASupprimer {
-	
-	public static void main(String[] args){
-		
-		System.out.println("Je suis Camille et j'ai réussi à me synchro avec ce projet! mais pas à fetch.. en m�me temps, il faut pas fetch, il faut pull ! CA MAAAAAARCHE!!!!");
-	}
-
-}
