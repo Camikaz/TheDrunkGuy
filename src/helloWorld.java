a6e5d5f220c88e63c16d439cca1d1a37437554ac
Camikaz <camille.rouet@insa-lyon.fr>
2015-04-22 22:09:41 +0200

    Update helloWorld.java

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -4,6 +4,6 @@
 public class helloWorld {
     public static void main (String[] args){
         System.out.println("hi bitches");
-        System.out.println("hehe");
+        System.out.println("hehea");
     }
 }
