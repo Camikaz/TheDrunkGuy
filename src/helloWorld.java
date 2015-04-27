277e57e8b7a2aa4f1a030526c1e93bc9d315d9da
AlesteamSCAN <alestair.wilson@insa-lyon.fr>
2015-04-27 09:35:11 +0200

    Update helloWorld.java

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -4,6 +4,6 @@
 public class helloWorld {
     public static void main (String[] args){
         System.out.println("hi bitches - TESTTTT");
-        System.out.println("hehea aaALEST");
+        System.out.println("hehea aaAaaaaaghgjtEST");
     }
 }
